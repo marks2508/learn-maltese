5d94b39d01ff9abcc9a107867fa505f48e595a80
Mark S <markscicluna@hotmail.com>
2018-08-17 09:27:36 +0100

    able to loop through words. going to re-do the card model and seeds file

--- src/Numbers.js
+++ src/Numbers.js
@@ -5,9 +5,9 @@ class Numbers extends React.Component {
 
     state = {
       words: []
-    };
+    }
 
-    componentWillMount() {
+    componentDidMount() {
       Axios
         .get('/api/cards')
         .then(res => this.setState({ words: res.data }, () => console.log(this.state.words[0].words[1].word))) // logs 'blue' which is correct
@@ -15,25 +15,30 @@ class Numbers extends React.Component {
     }
 
 
+
     render() {
+
+      let wordsArray = [];
+      this.state.words.forEach(x => x.words.forEach(y => wordsArray.push(y)));
 
 
       return (
         <div>
           <h1>Numberss</h1>
           <h3>dwdw</h3>
-          {this.state.words.map(word => {
+          {wordsArray.map(function (words, i) {
             return(
-              <div key={word.id} className="image-tile col-md-4 col-sm-6 col-xs-12">
-                <h3>Category: {word.category}</h3>
+              <div key={i} className="image-tile col-md-4 col-sm-6 col-xs-12">
+                {/* <h3>Category: </h3> */}
+                <h4>Word: {words.word}</h4>
+                <h4>Translation: {words.translation}</h4>
+
                 {/* <h5>Word: {this.state.words.words.word}</h5>
-                <h5>Translation: {this.state.words.words.translation}</h5> */}
+                  <h5>Translation: {this.state.words.words.translation}</h5> */}
+                {/* <h3>Loop: {wordsArray}</h3> */}
               </div>
             );
           })}
-
-
-
         </div>
 
       );
